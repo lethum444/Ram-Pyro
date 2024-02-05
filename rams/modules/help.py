2d9527f378cff4d07ff97a90e0d03711b02af4b8
lethum444 <154496698+lethum444@users.noreply.github.com>
2024-02-05 18:46:37 +0700

    help.py

--- rams/modules/help.py
+++ rams/modules/help.py
@@ -46,7 +46,7 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "Ram Pyro Modules"
+            ac.title = "Collas Pyro Modules"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
@@ -87,7 +87,7 @@ async def module_helper(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "Ram Pyro Modules"
+        ac.title = "Collas Pyro Modules"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
